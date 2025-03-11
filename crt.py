c4e72d2d3086bfa423f50e98ba51d416cb84ec24
Joe Makepeace <joe.makepeace01@gmail.com>
2025-03-11 20:38:08 +0000

    Working crt solver

--- crt.py
+++ crt.py
@@ -1,9 +1,31 @@
 """Understanding chinese remainder theorem (CRT)."""
 
-num = [5, 7]
-rem = [1, 3] # residues
+def findMinX(num, rem, k):
+    # Initialize result
+    x = 1
 
-# 31 smallest num that:
-# x % 5 = 1
-# x % 7 = 3
+    # As per the Chinise remainder
+    # theorem, this loop will
+    # always break.
+    while(True):
+        # Check if remainder of x % num[j] is rem[j] or not (for all j from 0 to k-1)
+        j = 0
+        while(j < k):
+            if (x % num[j] != rem[j]):
+                break
+            j += 1
 
+        # If all remainders 
+        # matched, we found x
+        if (j == k):
+            return x
+
+        # Else try next number
+        x += 1
+
+# Driver Code
+if __name__ == "__main__":
+    num = [5, 7] # moduli (divisors)
+    rem = [4, 2] # residues
+    k = len(num)
+    print("x is", findMinX(num, rem, k))
